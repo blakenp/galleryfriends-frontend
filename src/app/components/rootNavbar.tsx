49d1685d190fbb2380f4ca2918bd00c3e7a0ef7a
Blake Peterson <blake.ptrsn18@gmail.com>
2023-08-07 22:48:40 -0600

    Make hamburger styled menu render on mobile screens

--- src/app/components/rootNavbar.tsx
+++ src/app/components/rootNavbar.tsx
@@ -1,38 +1,68 @@
-// rootNavbar.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import styles from '../styles/navbar.module.css';
 import { MenuItems } from '../data/rootMenuItems';
 
 export default function RootNavbar() {
   const pathname = usePathname();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const handleHomeNavigation = () => {
     window.location.href = '/';
   };
 
-  return (
-    <div className={styles.navbar}>
-      <button className={styles.sitetitle} onClick={handleHomeNavigation}>
-        Gallery Friends
-      </button>
-      <nav className={styles.navbar}>
-        {MenuItems.map((item) => {
-          const isActive = pathname === item.url;
+  const handleMobileMenuToggle = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
 
-          return (
-            <Link key={item.title} href={item.url}>
-              <div
-                className={
-                  isActive ? styles.activeNavLink : styles.navLink
-                }
-              >
-                {item.title}
-              </div>
-            </Link>
-          );
-        })}
+  return (
+    <div className="bg-gray-900 text-white">
+      <div className="flex justify-between items-center p-4">
+        <button className="text-2xl font-semibold cursive" onClick={handleHomeNavigation}>
+          Gallery Friends
+        </button>
+        <nav className="hidden md:flex md:items-center md:space-x-6">
+          {MenuItems.map((item) => {
+            const isActive = pathname === item.url;
+
+            return (
+              <Link key={item.title} href={item.url}>
+                <div
+                  className={`${
+                    isActive ? 'text-white' : 'text-gray-300'
+                  } py-2 md:py-0 md:hover:text-white`}
+                >
+                  {item.title}
+                </div>
+              </Link>
+            );
+          })}
+        </nav>
+        <button
+          className="text-xl md:hidden"
+          onClick={handleMobileMenuToggle}
+        >
+          &#9776; {/* Hamburger icon */}
+        </button>
+      </div>
+      <nav className={`md:hidden ${mobileMenuOpen ? 'block' : 'hidden'}`}>
+      <div className="md:flex md:items-center md:flex-col md:p-4">
+          {MenuItems.map((item) => {
+            const isActive = pathname === item.url;
+
+            return (
+              <Link key={item.title} href={item.url}>
+                <div
+                  className={`${
+                    isActive ? 'text-white' : 'text-gray-300'
+                  } py-2 md:py-0 md:hover:text-white`}
+                >
+                  {item.title}
+                </div>
+              </Link>
+            );
+          })}
+        </div>
       </nav>
     </div>
   );
